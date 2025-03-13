f84c098bf3609e4d106ff634fbbe3b4dcae23a5f
JackSvensson <Jackens1997@gmail.com>
2025-03-13 10:18:52 +0100

    added styles.css

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bookshelf with Forty Books</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
+
 <body>
     <div class="bookshelf-container">
         <div class="bookshelf">
@@ -19,7 +22,7 @@
             <div class="book"></div>
             <div class="book"></div>
         </div>
-        
+
         <div class="bookshelf">
             <div class="book"></div>
             <div class="book"></div>
@@ -32,7 +35,7 @@
             <div class="book"></div>
             <div class="book"></div>
         </div>
-        
+
         <div class="bookshelf">
             <div class="book"></div>
             <div class="book"></div>
@@ -45,7 +48,7 @@
             <div class="book"></div>
             <div class="book"></div>
         </div>
-        
+
         <div class="bookshelf">
             <div class="book"></div>
             <div class="book"></div>
@@ -60,4 +63,5 @@
         </div>
     </div>
 </body>
+
 </html>
