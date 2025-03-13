b40c9b416e90da25f8e3ba846575132a848ba11c
Viktor-TPD <viktortohverstridh@gmail.com>
2025-03-13 10:46:24 +0100

    Books are looped through and displayed in the DOM.

--- index.php
+++ index.php
@@ -1,60 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
+<?php 
+require 'Bookshelf.php';
 
+$bookshelf = new Bookshelf();
+$allBooks = $bookshelf->getAllBooks();
+?>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bookshelf with Forty Books</title>
     <link rel="stylesheet" href="styles.css">
 </head>
-
+    <!-- <div class="bookshelf-container">
+        <div class="bookshelf">
+            <div class="book"></div> -->
 <body>
     <div class="bookshelf-container">
         <div class="bookshelf">
-            <div class="book"></div>
-            <div class="book"></div>
-            <div class="book"></div>
-            <div class="book"></div>
-            <div class="book"></div>
-            <div class="book"></div>
-            <div class="book"></div>
-            <div class="book"></div>
-            <div class="book"></div>
-            <div class="book"></div>
 
-
-            <div class="book"></div>
-            <div class="book"></div>
-            <div class="book"></div>
-            <div class="book"></div>
-            <div class="book"></div>
-            <div class="book"></div>
-            <div class="book"></div>
-            <div class="book"></div>
-            <div class="book"></div>
-            <div class="book"></div>
-
-            <div class="book"></div>
-            <div class="book"></div>
-            <div class="book"></div>
-            <div class="book"></div>
-            <div class="book"></div>
-            <div class="book"></div>
-            <div class="book"></div>
-            <div class="book"></div>
-            <div class="book"></div>
-            <div class="book"></div>
-
-            <div class="book"></div>
-            <div class="book"></div>
-            <div class="book"></div>
-            <div class="book"></div>
-            <div class="book"></div>
-            <div class="book"></div>
-            <div class="book"></div>
-            <div class="book"></div>
-            <div class="book"></div>
-            <div class="book"></div>
+            <?php
+            foreach ($allBooks as $book): 
+                ?><div class="book" style="height:<?= $book['pages'] ?>px; width:<?= $book['pages'] * 0.3 ?>px; min-width:50px; max-height:90vh;">
+                    <p><?= $book['name'] ?></p>
+                    <p><?= $book['author'] ?></p>
+                </div>
+            <?php
+            endforeach;
+            ?>
         </div>
     </div>
 </body>
